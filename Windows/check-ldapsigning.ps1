8f8f259c5ccd584f42928d7f4faa51d23756ed4d
ville87 <38942588+ville87@users.noreply.github.com>
2021-02-15 09:55:03 +0100

    Create check-ldapsigning.ps1

---
+++ Windows/check-ldapsigning.ps1
@@ -0,0 +1,16 @@
+# Ugly PS script to check if ldap signing is enforced on domain controllers in the current domain
+# 
+# get all dcs in the domain
+$dcs = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().DomainControllers
+# to test with the currently logged on user, env variables are being used, otherwise change this to e.g. hardcoded user
+$username = "$env:userdomain\$env:username"
+$credentials = new-object "System.Net.NetworkCredential" -ArgumentList $UserName,(Read-Host "Password" -AsSecureString)
+# for every dc in the domain, check if you can connect with basic auth, to see if LDAP signing is enforced or not
+foreach($dc in $dcs){
+  $hostname = $dc.Name
+  $Null = [System.Reflection.Assembly]::LoadWithPartialName("System.DirectoryServices.Protocols")
+  $LDAPConnect = New-Object System.DirectoryServices.Protocols.LdapConnection "$HostName"
+  $LdapConnect.AuthType = [System.DirectoryServices.Protocols.AuthType]::Basic
+  $LDAPConnect.Bind($credentials)
+  if(($error[0] | select-string -Pattern "Strong authentication is required for this operation") -ne $null){ write-output "[OK] LDAP Signing is enforced on domain controller $hostname"} else { write-output "[WARN] LDAP Signing is not enforced on domain controller $hostname"}
+}
