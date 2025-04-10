900065d12bce8a42c466306a5325b77ad4157055
ville87 <38942588+ville87@users.noreply.github.com>
2025-04-10 11:52:17 +0300

    Update ADSI_Enum_ADIDNS.ps1

--- Windows/ADSI_Enum_ADIDNS.ps1
+++ Windows/ADSI_Enum_ADIDNS.ps1
@@ -1,5 +1,4 @@
 # WIP, TODO: 
-# - Check why so called "legacy DNS zone entries" are not found this way...
 # - Improve the credential handling, cause its ugly now...
 $search = [adsisearcher]"(&(objectClass=DnsZone))"
 $domain = new-object DirectoryServices.DirectoryEntry("LDAP://10.0.0.1","domain\username", "Password")
@@ -22,4 +21,32 @@ foreach($dnszoneobj in $dnszoneobjects){
     }
 }
 
+# ------------------------------------------------------------
+$dc = "10.0.1.10"
+$username = "domain\user"
+$password = "password"
 
+$zonePath = "LDAP://$dc/DC=DomainDNSZones,DC=testlab,DC=local"
+$zoneEntry = New-Object DirectoryServices.DirectoryEntry($zonePath, $username, $password)
+
+$searcher = New-Object DirectoryServices.DirectorySearcher($zoneEntry)
+$searcher.Filter = "(objectClass=dnsNode)"
+$searcher.PageSize = 1000
+$searcher.PropertiesToLoad.Add("name") | Out-Null
+$searcher.PropertiesToLoad.Add("dnsRecord") | Out-Null
+$searcher.PropertiesToLoad.Add("distinguishedName") | Out-Null
+
+$results = $searcher.FindAll()
+foreach($result in $results ){
+    # Sample dnsRecord (this is the raw binary data)
+    $dnsRecord = $result.Properties["dnsrecord"][0]
+
+    # Extract the last 4 bytes (IPv4 address), which are usually the last part of an A record
+    $ipAddressBytes = $dnsRecord[-4..-1]  # Get the last 4 bytes
+
+    # Convert the byte array to a human-readable IP address
+    $ipAddress = [System.Net.IPAddress]::new($ipAddressBytes)
+
+    # Output the human-readable IP Address
+    Write-Host "$($result.properties.name) has address: $ipAddress"
+}
